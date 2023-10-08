bbbabd765af7db85a772eaa32870490e339339b8
Leslie Lopez <leslie2515lopez@gmail.com>
2023-10-07 23:48:56 -0600

    Updated file

--- controllers/image.js
+++ controllers/image.js
@@ -1,38 +1,12 @@
-const handleApiCall = (req, res, fetch) => {
-  const imageUrl = req.body.input;
-  const returnClarifaiRequestOptions = (url) => {
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: "pp9glgunpdb5",
-        app_id: "c833fb8738c54b64aef624046587f201",
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: url,
-            },
-          },
-        },
-      ],
-    });
+const Clarifai = require("clarifai");
 
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: process.env.CLARIFAI_API_KEY,
-      },
-      body: raw,
-    };
-    return requestOptions;
-  };
+const app = new Clarifai.App({
+  apiKey: process.env.API_CLARIFAI,
+});
 
-  fetch(
-    `https://api.clarifai.com/v2/models/face-detection/outputs`,
-    returnClarifaiRequestOptions(imageUrl)
-  )
-    .then((response) => response.text())
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then((data) => {
       res.json(data);
     })
