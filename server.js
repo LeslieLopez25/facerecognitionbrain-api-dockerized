f9212c08ab3c330069e6939e244b99212326606d
Leslie Lopez <leslie2515lopez@gmail.com>
2023-10-07 23:36:19 -0600

    Updated image.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const fetch = require("node-fetch");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
@@ -45,7 +46,7 @@ app.put("/image", (req, res) => {
 });
 
 app.post("/imageurl", (req, res) => {
-  image.handleApiCall(req, res);
+  image.handleApiCall(req, res, fetch);
 });
 
 app.listen(process.env.PORT || 3000, () => {
