cce70eeb16eaf2f1f42b8b815ec78721537a4138
Leslie Lopez <leslie2515lopez@gmail.com>
2023-11-07 19:37:55 -0600

    Retrieving auth token

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,7 +3,7 @@ const redis = require("redis");
 
 // setup Redis:
 const redisClient = redis.createClient({
-  host: "https://facerecognitionbrain-api-ral3.onrender.com",
+  host: "https://facerecognitionbrain-api-ral3.onrender.com/signin",
 });
 
 const handleSignin = (req, res, db, bcrypt) => {
@@ -33,26 +33,40 @@ const handleSignin = (req, res, db, bcrypt) => {
     .catch((err) => Promise.reject("wrong credentials"));
 };
 
-const getAuthTokenId = () => {
-  console.log("auth ok");
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(400).json("Unauthorized");
+    }
+    return res.json({ id: reply });
+  });
 };
 
 const signToken = (email) => {
   const jwtPayload = { email };
-  return jwt.sign(jwtPayload, "JWT_SECRET", { expiresIn: "1 day" });
+  return jwt.sign(jwtPayload, "JWT_SECRET", { expiresIn: "1h" });
+};
+
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value));
 };
 
 const createSessions = (user) => {
   // JWT token, return user data
   const { email, id } = user;
   const token = signToken(email);
-  return { success: "true", userId: id, token };
+  return setToken(token, id)
+    .then(() => {
+      return { success: "true", userId: id, token };
+    })
+    .catch(console.log);
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization
-    ? getAuthTokenId()
+    ? getAuthTokenId(req, res)
     : handleSignin(req, res, db, bcrypt)
         .then((data) => {
           return data.id && data.email
